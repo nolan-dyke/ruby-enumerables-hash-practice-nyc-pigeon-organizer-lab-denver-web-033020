e1b7c228d2424d68978dcad8ae14b986bd98945d
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-03-19 19:02:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,55 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  names = data[:gender]
+  result = {}
+  i = 0 
+  while i < names[:male].length do 
+    result[names[:male][i]] = {:color => [], :gender => ["male"], :lives => []}
+    i += 1 
+  end 
+  i = 0
+  while i < names[:female].length do 
+    result[names[:female][i]] = {:color => [], :gender => ["female"], :lives => []}
+    i += 1 
+  end 
+  i = 0 
+  while i < data[:color][:purple].length do 
+    name = data[:color][:purple][i] 
+    if result[name]
+      result[name][:color] << "purple"
+    end 
+    i += 1 
+  end 
+  i = 0 
+  while i < data[:color][:grey].length do 
+    name = data[:color][:grey][i] 
+    if result[name]
+      result[name][:color] << "grey"
+    end 
+    i += 1 
+  end 
+  i = 0 
+  while i < data[:color][:white].length do 
+    name = data[:color][:white][i] 
+    if result[name]
+      result[name][:color] << "white"
+    end 
+    i += 1 
+  end 
+  i = 0 
+  while i < data[:color][:brown].length do 
+    name = data[:color][:brown][i] 
+    if result[name]
+      result[name][:color] << "brown"
+    end 
+    i += 1 
+  end 
+  i = 0 
+  while i < data[:lives][:subway].length do 
+    name = data[:lives][:subway][i] 
+    if result[name]
+      result[name][:lives] << "Subway"
+    end 
+    i += 1 
+  end 
+  result
 end
